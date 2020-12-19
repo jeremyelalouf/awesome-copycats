ff7030030ac76214fcea1b3c425c1a666aa29c7f
Phantomas <phantomas@phantomas.xyz>
2020-12-19 14:53:06 +0100

    [Add] Use common mode

--- modes/init.lua
+++ modes/init.lua
@@ -1,10 +1,9 @@
-local common = require("modes.common")
-
 local modes = {
     normal    = require("modes.normal"),
     tag       = require("modes.tag"),
     visual    = require("modes.visual"),
-    launcher  = require("modes.launcher")
+    launcher  = require("modes.launcher"),
+    common    = require("modes.common")
 }
 
 local ignores = {
@@ -12,13 +11,5 @@ local ignores = {
     visual    = 'v',
     launcher  = 'r'
 }
-
-for mode, desc in pairs(modes) do
-    for _, v in ipairs(common) do
-        if table.concat(v.pattern) ~= ignores[mode] then
-            table.insert(desc, v)
-        end
-    end
-end
 
 return modes
