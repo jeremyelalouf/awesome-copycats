92f4afec4853f4875f8fa6184e9ad6ace875996d
Phantomas <phantomas@phantomas.xyz>
2021-09-03 13:39:38 +0200

    [UPD] Update submodule modalawesome

--- modes/init.lua
+++ modes/init.lua
@@ -1,15 +1,10 @@
 local modes = {
-    normal    = require("modes.normal"),
-    tag       = require("modes.tag"),
-    visual    = require("modes.visual"),
-    launcher  = require("modes.launcher"),
-    common    = require("modes.common")
-}
-
-local ignores = {
-    tag       = 'T',
-    visual    = 'v',
-    launcher  = 'r'
+    normal      = require("modes.normal"),
+    tag         = require("modes.tag"),
+    visual      = require("modes.visual"),
+    launcher    = require("modes.launcher"),
+    mouse       = require("modes.mouse"),
+    common      = require("modes.common")
 }
 
 return modes
